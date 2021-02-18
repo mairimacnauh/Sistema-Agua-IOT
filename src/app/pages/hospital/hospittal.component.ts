ffabc3c27de0845bdf4dff43c0218e0a0afa0a8a
Miriam Mamani <mairim.acnauh@gmail.com>
2021-02-17 21:31:52 -0500

    styless

--- src/app/pages/hospital/hospittal.component.ts
+++ src/app/pages/hospital/hospittal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-estadistica',
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HospittalComponent implements OnInit {
 
-  constructor() { }
+  dataHosp: any = [];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.dataHosp = [
+      {
+        image: 'https://randomuser.me/api/portraits/men/30.jpg',
+        name: 'Dean Lynch',
+        createdBy: '',
+        createdDate: '05/06/2020',
+        lastModified: '10:00'
+      },
+      {
+        image: 'https://randomuser.me/api/portraits/men/76.jpg',
+        name: 'Ralph Barnes',
+        createdBy: '',
+        createdDate: '05/06/2020',
+        lastModified: '10:00'
+      },
+      {
+        image: 'https://randomuser.me/api/portraits/men/38.jpg',
+        name: 'Brett Castillo',
+        createdBy: '',
+        createdDate: '05/06/2020',
+        lastModified: '10:00'
+      },
+      {
+        image: 'https://randomuser.me/api/portraits/men/32.jpg',
+        name: 'Antony Barnes',
+        createdBy: '',
+        createdDate: '05/06/2020',
+        lastModified: '08:35'
+      },
+      {
+        image: 'https://randomuser.me/api/portraits/men/34.jpg',
+        name: 'Fernando Barnes',
+        createdBy: '',
+        createdDate: '05/06/2020',
+        lastModified: '10:00'
+      },
+      {
+        image: 'https://randomuser.me/api/portraits/men/36.jpg',
+        name: 'Ramiro Barnes',
+        createdBy: '',
+        createdDate: '05/06/2020',
+        lastModified: '10:00'
+      },
+    ];
   }
 
 }
